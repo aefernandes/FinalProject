012b4af5db8b042ad446531e9ac81068f62cfb33
Raul Jordan <rauljordan@college.harvard.edu>
2014-04-17 19:58:57 -0400

    started skeleton for the project. Requires nosetests package

---
+++ tests/Dataminer_tests.py
@@ -0,0 +1,12 @@
+from nose.tools import *
+from Dataminer.skiplistclass import SkipList
+
+# creates a testing suite for my project
+def setup():
+    print "SETUP"
+
+def teardown():
+    print "Tear Down"
+
+def test_basic():
+    print "Program runs"
