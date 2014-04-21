270f26f84eed69c78d195fa101b82779189da966
Raul Jordan <rauljordan@college.harvard.edu>
2014-04-21 18:37:22 -0400

    started csv parsing

--- Dataminer/population.py
+++ Dataminer/population.py
@@ -4,7 +4,8 @@ import csv
 
 class Population(SkipList):
 	"""docstring for Population"""
-	def __init__(self, state, name):
+	def __init__(self, csvfile):
+		self.csvfile = csvfile
 
 # parse the csv, and create a list of dictionaries
 # each element has a key of a state ID, and the values
@@ -14,7 +15,17 @@ class Population(SkipList):
 # define the state ID's for the user lookup later
 
 # modularizing a buildPopulation function to simplify the code in main.py
-	def buildPopulation(file):
+	def buildPopulation(self):
 		# opens the csv file and this is the main method that builds 
 		# the skiplist of dicts for population
-		with open(file) as f:
+		with open(self.csvfile) as f:
+			population = csv.reader(f)
+			for row in population:
+				# insert row[3] as keys of a dict
+				# insert rest of names as keys, and rest of everything
+				# as values
+
+if __name__ == '__main__':
+	popobj = Population('population.csv')
+	popobj.buildPopulation()
+
