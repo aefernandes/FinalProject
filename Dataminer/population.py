f978d905493b14052fb867e480ed073b246e60b9
Raul Jordan <rauljordan@college.harvard.edu>
2014-04-21 21:45:27 -0400

    POPULATION WORKS

--- Dataminer/population.py
+++ Dataminer/population.py
@@ -14,13 +14,14 @@ class Population(SkipList):
 # for each in dictlist, insert each as a node. Return the skiplist 
 # define the state ID's for the user lookup later
 
-# modularizing a buildPopulation function to simplify the code in main.py
 	def buildPopulation(self):
 		# opens the csv file and this is the main method that builds 
 		# the skiplist of dicts for population
 		dictlist = []
 		with open(self.csvfile) as f:
 			population = csv.reader(f)
+
+			headers = next(population)
 
 			for row in population:
 				# popdict is a dictionary for each row, in which the key is
@@ -30,17 +31,20 @@ class Population(SkipList):
 				# rest of dict contains all other items of the row except for # state ID
 				restofdict = {}
 
+				# builds the restofdict using header names as keys
+				# deletes unnecessary state ID key, value pair
 				for i in range(len(row)):
-					restofdict[row[i]] = row[i]
-					if row[3] in restofdict: 
-						del restofdict[row[3]]
+					restofdict[headers[i]] = row[i]
+					if 'STATE' in restofdict: 
+						del restofdict['STATE']
 
 				popdict[row[3]] = restofdict
+				# appends to the list of dictionaries
 				dictlist.append(popdict)
 			# deletes the unnecessary first row of headers in final list
 			del dictlist[0]
 
-		print dictlist
+
 
 if __name__ == '__main__':
 	popobj = Population('population.csv')
